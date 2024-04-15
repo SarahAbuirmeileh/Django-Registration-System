3501a6166a2d79ce9e3da7fbffe93d6de3deb030
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-04-15 14:39:42 +0200

    Create an api to get all details about course

--- course_reg_system/courses/views.py
+++ course_reg_system/courses/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .models import Course
+from .serializers import CourseDetailSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def course_details(request, course_id):
+    try:
+        course = Course.objects.get(pk=course_id)
+    except Course.DoesNotExist:
+        return Response({"message": "Course not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = CourseDetailSerializer(course)
+    return Response(serializer.data)
+
