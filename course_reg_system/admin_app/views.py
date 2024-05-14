189383c3130ec5a73d101264f4ba0d7c2ac3548d
AsiaShalaldeh <arwwd2002@gmail.com>
2024-05-14 19:50:40 +0300

    Edit Report view

--- course_reg_system/admin_app/views.py
+++ course_reg_system/admin_app/views.py
@@ -13,6 +13,8 @@ from .serializers import CourseSerializer
 from students.serializers import DeadlineSerializer
 from students.models import Student, Deadline
 from schedules.models import CourseSchedule
+from rest_framework.response import Response
+
 
 @api_view(['DELETE', 'POST', 'GET'])
 def delete_edit_course(request, course_id):
@@ -55,18 +57,15 @@ def delete_edit_course(request, course_id):
         return redirect('home') 
 
 
-from rest_framework.response import Response
-from rest_framework import status
-from django.http import Http404
-
 @api_view(['GET'])
 def generate_reports(request):
-<<<<<<< HEAD
     if request.method == 'GET':
         try:
-            # Query all students registered for courses
+            # if request.user.is_staff:
+                # Query all students registered for courses
             student_regs = StudentRegistration.objects.all()
 
+            # Calculate total number of students registered for each course
             courses_enrollments = {}
             for reg in student_regs:
                 try:
@@ -79,7 +78,7 @@ def generate_reports(request):
                             courses_enrollments[course_code] = 1
                 except Exception as e:
                     print(f"Error processing registration {reg.registration_id}: {e}")
-                    
+                        
             # Query course details
             courses = Course.objects.all()
 
@@ -97,64 +96,15 @@ def generate_reports(request):
                 # Add more data for other relevant reports
             }
             return render(request, 'courses_reports.html', data)
+            # else:
+            #     # Return Forbidden response if user is not staff
+            #     return Response({'message': 'Permission denied.'}, status=status.HTTP_403_FORBIDDEN)
         except StudentRegistration.DoesNotExist:
+            # Return response if no students are registered
             return render(request, 'courses_reports.html', {'message': 'No students registered for courses.'})
     else:
+        # Return Method Not Allowed response for other request methods
         return Response({'message': 'Method not allowed.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-=======
-
-    if request.user.is_staff:
-    
-        # Query all students registered for courses
-        student_regs = StudentRegistration.objects.all()
-
-        # Calculate total number of students registered for each course
-        courses_enrollments = {}
-        for reg in student_regs:
-            course_code = reg.course.course_code
-            if course_code in courses_enrollments:
-                courses_enrollments[course_code] += 1
-            else:
-                courses_enrollments[course_code] = 1
-
-        # Query course details
-        courses = Course.objects.all()
-
-        # Calculate course popularity based on some factors
-        courses_popularity = {}
-        for course in courses:
-            # Calculating popularity based on number of enrollments
-            popularity_score = courses_enrollments.get(course.course_code, 0)
-            # We can add more factors here to determine course popularity
-            courses_popularity[course.course_code] = popularity_score
-
-            # Additional factors to consider
-            # Factor 1: Course ratings
-            # Assuming the Course model has a field for ratings
-            # if course.ratings:
-            #     popularity_score += course.ratings
-
-            # Factor 2: Instructor reputation
-            # Assuming the Course model has a field for instructor reputation
-            # if course.instructor_reputation:
-            #     popularity_score += course.instructor_reputation
-
-            # Factor 3: Student feedback
-            # Assuming we have a way to collect and analyze student feedback
-            # We can calculate a score based on feedback and add it to popularity_score
-
-        data = {
-            'courses_enrollments': courses_enrollments,
-            'courses_popularity': courses_popularity,
-            # Add more data for other relevant reports
-        }
-        # return Response(data)
-        return render(request, 'courses_reports.html', data)
-    
-    else:
-        return redirect('home') 
->>>>>>> 876260d02931e65b336a6f7158f3c72a4b70ed84
 
 
 @api_view(['POST', 'GET'])
