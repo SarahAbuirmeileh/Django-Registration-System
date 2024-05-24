12fb8041ff5c220b55ee95bd601e61ad8142778a
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-05-24 11:10:41 +0300

    Write view to get deadlines and display it in html page

--- course_reg_system/students/urls.py
+++ course_reg_system/students/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('student/<int:id>/', views.student_details, name='delete_student'),
     path('student/course/<int:course_code>/', views.add_course_to_schedule,name='add_course_to_schedule'),
     path('student/<int:student_id>/schedule/', views.get_student_schedule, name='get_student_schedule'),
-    path('student/deadline/', views.student_deadlines, name='get_student_deadlines'),
+    path('deadline/', views.get_deadlines, name='get_deadlines'),
 ]
